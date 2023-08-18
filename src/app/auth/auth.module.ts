da689877bf331a9ae568ad218d8f9a92240e12ee
shailraj368 <shailrajrtridhyatech@gmail.com>
2023-08-18 10:40:38 +0530

    Register form validation

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { RegisterComponent } from './register/register.component';
 import { FormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
-  imports: [CommonModule, FormsModule,AuthRoutingModule],
+  imports: [CommonModule, FormsModule,AuthRoutingModule,ReactiveFormsModule],
 })
 export class AuthModule {}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,35 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { UserAuthService } from '../service/user-auth.service';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
-  constructor(private service: UserAuthService, private router: Router) {}
+export class RegisterComponent implements OnInit {
+  registrationForm!: FormGroup;
 
-  user = {
-    name: '',
-    userName: '',
-    password: '',
-    role: '',
-  };
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: UserAuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.registrationForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, this.passwordValidator]],
+      role: ['', Validators.required],
+    });
+  }
 
   onSubmit() {
-    this.service.registerUser(this.user).subscribe({
-      next: (res) => {
-        // this.service.refreshList();
-        // window.location.reload();
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+    if (this.registrationForm.valid) {
+      const userData = this.registrationForm.value;
+      this.service.registerUser(userData).subscribe({
+        next: (res) => {
+          // Handle success
+          this.router.navigate(['WorkHistory/list']);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }
+  }
 
-    console.log(this.user, 'Register.....');
-    this.router.navigate(['WorkHistory/list']);
+  passwordValidator(
+    control: AbstractControl
+  ): { [key: string]: boolean } | null {
+    const value = control.value;
+    const hasNumber = /[0-9]/.test(value);
+    const hasLetter = /[a-zA-Z]/.test(value);
+    const hasSpecialChar = /[^a-zA-Z0-9]/.test(value);
+    const valid = hasNumber && hasLetter && hasSpecialChar && value.length >= 6;
+
+    if (!valid) {
+      return { invalidPassword: true };
+    }
+
+    return null;
   }
 }
